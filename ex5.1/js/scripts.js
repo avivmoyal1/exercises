5353e404208785bc3d868c1d75709e7e1b3f686b
avivmoyal1 <avivmoyal1@gmail.com>
2022-05-02 11:28:41 +0300

    aviv id

--- ex5.1/js/scripts.js
+++ ex5.1/js/scripts.js
@@ -65,13 +65,13 @@ function myFunction() {
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function dropMenu(e) {
+    document.getElementById(e).classList.toggle("show");
   }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
-    if (!event.target.matches('#dropbtn')) {
+    if (!event.target.matches('#dropbtn') ) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
